693cd374c67b1cfd19d69ee76e10dbc46c637a27
Chenxiaolong <chenxiaolong@youku.com>
2016-10-10 15:23:42 +0800

    提交测试工程

---
+++ Block/Block/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Block
+//
+//  Created by Carouesl on 2016/10/10.
+//  Copyright © 2016年 Carouesl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
